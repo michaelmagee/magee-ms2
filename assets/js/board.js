dd7ead85ab53ee8269198e9b25644bdeb0e98b48
michaelmagee <59846293+michaelmagee@users.noreply.github.com>
2020-07-28 11:39:42 -0400

    Initial add of the Board class that has been prototyped  includes initial card creation and listener association.

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,9 +1,12 @@
-/* 
-Class: Board
-This class will hold all of the functionality that is closely related to the card and it's oeration
-
-*/
-
+/**
+ * @class: Board
+ * @classdesc: Represents a Board.  Manages cards.
+ * @constructs
+ * @param {Game} game - The Game object parent.
+ * @param {string} type - The level type of the card.
+ * @param {string} cardcount - Number of cards in the game.
+ * 
+ */
 class Board {
 
     constructor(game, type, cardcount) {
@@ -11,29 +14,43 @@ class Board {
         this.type = type;
         this.cardCount = cardcount;
         this.cards = [];
-        this.boardElement = document.getElementById("game-board");
-        console.log("constructor for Board completed ");
+        this.boardElement = document.getElementById("gameboard");
+        console.log("constructor for Board completed ");                /* Remove  */
     }
 
+/**
+ * @method: cardFlip
+
+ * @param {event} event - The Game object parent.
+ * Processes a card click event
+ */
     cardFlip(event) {
-        let clickedCard = this.getCardByID(event.id);
-        let hasittest = event.classList.contains("wiggle");
-        let nopetest = event.classList.contains("gum");
-        this.wiggle();
-        console.log("Clicked: " + clickedCard.innerHtml);
+        $(`#${event.id}`).toggleClass("click");
+        $(`#${event.id}`).css("transform, rotateY(180deg)");
+
+        console.log("Clicked: " + event.id);
     }
 
+    /**
+ * @method: wiggle
+ * 
+ * @param {card} card - The card that needs to be wiggled.  Planned for Hints
+ * 
+ * Wiggle a card as a match hint
+ */
     wiggle() {
+        /*
         $("#card-easy-1").addClass("wiggle");
-        $("#card-easy-2").addClass("wiggle");
-        console.log("wiggling on");
-        setTimeout(() => {
-            $("card-easy-1").removeClass("wiggle");
-            $("card-easy-2").removeClass("wiggle");
-        }, 700);
-        console.log("wiggling off");
+        */
     }
 
+/**
+ * @method: getCardByID
+ * 
+ * @param {card} card - The card that needs to be wiggled.  Planned for Hints
+ * @returns {text}  htmlID of card
+ * Wiggle a card as a match hint
+ */
     getCardByID(htmlId) {
         let foundCard = null;
         for (var i = 0; i < this.cards.length; i++) {
@@ -45,17 +62,27 @@ class Board {
         return foundCard;
     }
 
-
+/**
+ * @method: addCard
+ * 
+ * @param {card} card - Creates a card instance
+ * @returns {card}  card instance
+ * Adds a card  
+ */
     addCard(board, type, htmlId, cardValue) {
         let newCard = new Card(board, type, htmlId, cardValue);
         this.cards.push(newCard);
         return newCard;
     }
 
+    /**
+ * @method: addCards
+ * 
+ * uses defined card count to create all new cards PARAMS TBD 
+ */
     addCards() {
-
         for (var i = 1; i <= this.cardCount; i++) {
-            let card = this.addCard(this, this.type, `card-${this.type}-${i}`, `Value-${i}`);
+            let card = this.addCard(this, this.type, `card-${this.type}-${i}`, `V-${i}`);
             this.boardElement.innerHTML += card.getHTML();
             card.htmlElement = document.getElementById(card.htmlId);
             card.hello();
@@ -63,16 +90,23 @@ class Board {
 
     }
 
-
+    /**
+ * @method: addAllListeners
+ * 
+ * Adds all click listers to the cards 
+ * Note the was listenrs needed to be associated.  
+ */
     addAllListeners() {
-        let htmlCards = Array.from(document.getElementsByClassName('card-inline-grid'));
+        let htmlCards = Array.from(document.getElementsByClassName('flip-card'));
         htmlCards.forEach(card => {
             card.addEventListener('click', () => {
                 this.cardFlip(card);
             });
         });
 
-        /*
+        /*  CLEAN UP.  This was all of the unsuccessful attempst to add an event listener to a 2 deep level class.  
+        Document this 
+
         this.cards.forEach(card => {
             card.htmlElement.addEventListener('click', () => {
                 card.board.cardFlip(card);
